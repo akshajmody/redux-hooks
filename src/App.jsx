041a67ada6a24b737c238bb9ed4184ca3df33aa3
Akshaj Mody <akshajmody@gmail.com>
2021-01-08 01:31:48 +0900

    add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import AccountStatus from './components/AccountStatus'
+import Auth from './components/Auth'
+import Balance from './components/Balance'
+import Banking from './components/Banking'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
-  )
-}
+  <div>
+    <Auth/>
+    <Balance/>
+    <Banking/>
+    <AccountStatus/>
+  </div>
+  );
+};
 
 export default App
