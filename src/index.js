7bb819920f58bfd92f56152581aca1106a3658c3
Akshaj Mody <akshajmody@gmail.com>
2021-01-08 03:47:26 +0900

    add actions

--- src/index.js
+++ src/index.js
@@ -6,4 +6,31 @@ import {bankingReducer} from "./reducers/bankingReducers";
 
 const store = createStore(bankingReducer);
 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//action
+//one absolute requirement for action - thats a type parameter
+
+
+//deposit
+const deposit = {
+  type: "DEPOSIT",
+  amount: 20
+}
+
+//withdraw
+const withdraw = {
+  type: "WITHDRAW",
+  amount: 30
+}
+
+//collect interest
+
+const collectInterest = {
+  type: "COLLECT_INTEREST"
+  //Since we are going to providing a fixed interest amount, we don't need to add payload here and can implement the logic of the percentage within the reducer itself rather than the action
+
+}
