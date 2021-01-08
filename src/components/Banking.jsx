e2a94628103975cabfeb546f31195f6344e0affe
Akshaj Mody <akshajmody@gmail.com>
2021-01-08 20:33:25 +0900

    add onclick functions for banking component

--- src/components/Banking.jsx
+++ src/components/Banking.jsx
@@ -4,11 +4,11 @@ const Banking = () => {
   return (
     <div className="form-group">
       <input type="text" className="form-control"/>
-      <button className="btn btn-success">Deposit</button>
-      <button className="btn btn-primary">Withdraw</button>
-      <button className="btn btn-warning">Collect Interest</button>
-      <button className="btn btn-danger">Delete Account</button>
-      <button className="btn btn-secondary">Change Account Type</button>
+      <button onClick={handleDeposit} className="btn btn-success">Deposit</button>
+      <button onClick={handleWithdraw} className="btn btn-primary">Withdraw</button>
+      <button onClick={handleCollectInterest} className="btn btn-warning">Collect Interest</button>
+      <button onClick={handleDelete} className="btn btn-danger">Delete Account</button>
+      <button onClick={handleAccountChange} className="btn btn-secondary">Change Account Type</button>
     </div>
   )
 }
